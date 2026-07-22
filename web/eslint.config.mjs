3a0856ff02e9299e956c8674a2c6ec1d27583c79
Sérgio Dantas <sergiodnts828@gmail.com>
2026-07-22 11:15:54 -0300

    feat(web): add eslint prettier integration and format scripts

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  eslintConfigPrettier,
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
 ]);
 
 export default eslintConfig;
